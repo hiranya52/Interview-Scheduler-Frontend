a8332c35f3023ebdbeecbf68a0777ab402749740
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-18 11:28:44 +0530

    Add schedule overview header

--- interview-scheduler/src/app/pages/home/home.ts
+++ interview-scheduler/src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SideBar } from "../../component/side-bar/side-bar";
+import { Header } from "../../component/header/header";
 
 @Component({
   selector: 'app-home',
-  imports: [SideBar],
+  imports: [SideBar, Header],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
