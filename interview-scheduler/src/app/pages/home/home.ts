5366047ed72e2adadb233d384d01d46abad4fddb
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-18 11:35:32 +0530

    Add interview schedule filters with view toggle

--- interview-scheduler/src/app/pages/home/home.ts
+++ interview-scheduler/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SideBar } from "../../component/side-bar/side-bar";
 import { Header } from "../../component/header/header";
+import { Filters } from "../../component/filters/filters";
 
 @Component({
   selector: 'app-home',
-  imports: [SideBar, Header],
+  imports: [SideBar, Header, Filters],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
